3a29bd797ca379c2094f49edb7fdd44366862f9a
VinceMarkArnedo <59438164+VinceMarkArnedo@users.noreply.github.com>
2021-07-13 08:39:37 +0800

    Testing hosting web portoflio

---
+++ indexOriginal.php
@@ -0,0 +1,221 @@
+
+
+
+    <!-- <div class="App">
+      <nav  class="navbar">
+          <h1>VINCE ARNEDO</h1>
+          <span></span>
+          <a href="#">Projects</a>
+          <a href="#">About</a>
+          <a href="#">Contact</a>
+      </nav>
+
+    </div> -->
+
+    <!-- Navbar-->
+      <?php include 'ResponsiveNavbar.html';?>
+    <!-- Navbar-->
+
+    <section id="about">
+        <div class="about">
+            <img src="Images/hahaha.jpg" alt="My Pic">
+            <div class="introduction">
+                <!-- <h2>I'm <strong>Vince Mark E. Arnedo</strong></h2></br> --></br>
+                <h1>Converting caffeine and ideas into Software Products</h1>
+                <p>Lorem ipsum , I'm <span><bold> Vince Mark E. Arnedo</bold></span> amet consectetur adipisicing elit. Aliquid hic aperiam ad quae, quia officia sit. Officiis qui deserunt iure eaque hic illum expedita, ratione repellat molestias atque iste velit, delectus ut magni sit ab ducimus dolore nobis, eius ullam eveniet? Quam, at adipisci quae delectus quia eveniet officia. Quam iusto tenetur blanditiis dolores eos doloribus, odio numquam</p>
+                    </br></br>
+                <p>
+                    There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. 
+                  </p>
+            </div>
+        </div>
+    </section>
+
+    <!-- PORTFOLIO--> 
+    <div class="portfolio">
+      <h1>PROJECTS</h1>
+      <div class="cards">
+          <div class="card">
+              <!-- <img src="Images/SJ.JPG" alt=""> -->
+              <div class="content">
+                  <h2>Guidance Information System</h2>
+                  <img src="Images/JHGIS.JPG" alt="">
+                  <p>Quam iusto tenetur blanditiis dolores eos doloribus, odio numquam architecto alias amet consequatur, quae eius corporis, officiis fugit temporibus reiciendis. Beatae nobis molestias tempore omnis consequuntur hic qui ducimus iure nostrum. Aperiam ab magnam sit iusto excepturi perferendis aliquid, recusandae autem ipsum accusantium, itaque aliquam laborum est quidem facere culpa, atque a numquam neque fugiat delectus. Dicta inventore molestiae ipsa hic vero explicabo temporibus dolores fuga voluptatibus. </p>
+                  <!-- <a href="#" class="button">Read More</a> -->
+                </div>
+  
+          </div>
+          <!-- -->
+  
+          <div class="card">
+            <!-- <img src="Images/SJ.JPG" alt=""> -->
+            <div class="content">
+                <h2>Pizza House Customer Service</h2>
+                <img src="Images/PizzaHouse.JPG" alt="">
+                <p>Quam iusto tenetur blanditiis dolores eos doloribus, odio numquam architecto alias amet consequatur, quae eius corporis, officiis fugit temporibus reiciendis. Beatae nobis molestias tempore omnis consequuntur hic qui ducimus iure nostrum. Aperiam ab magnam sit iusto excepturi perferendis aliquid, recusandae autem ipsum accusantium, itaque aliquam laborum est quidem facere culpa, atque a numquam neque fugiat delectus. Dicta inventore molestiae ipsa hic vero explicabo temporibus dolores fuga voluptatibus. </p>
+                <!-- <a href="#" class="button">Read More</a> -->
+              </div>
+  
+        </div>
+        <!-- -->
+  
+        <div class="card">
+          <!-- <img src="Images/SJ.JPG" alt=""> -->
+          <div class="content">
+              <h2>Personal Productivity Tracker</h2>
+              <img src="Images/HT.JPG" alt="">
+              <p>Quam iusto tenetur blanditiis dolores eos doloribus, odio numquam architecto alias amet consequatur, quae eius corporis, officiis fugit temporibus reiciendis. Beatae nobis molestias tempore omnis consequuntur hic qui ducimus iure nostrum. Aperiam ab magnam sit iusto excepturi perferendis aliquid, recusandae autem ipsum accusantium, itaque aliquam laborum est quidem facere culpa, atque a numquam neque fugiat delectus. Dicta inventore molestiae ipsa hic vero explicabo temporibus dolores fuga voluptatibus. </p>
+              <!-- <a href="#" class="button">Read More</a> -->
+            </div>
+  
+      </div>
+      <!-- -->
+  
+      <div class="card">
+        <!-- <img src="Images/SJ.JPG" alt=""> -->
+        <div class="content">
+            <h2>Taj Hotel Management</h2>
+            <img src="Images/TajHome.JPG" alt="">
+            <p>Quam iusto tenetur blanditiis dolores eos doloribus, odio numquam architecto alias amet consequatur, quae eius corporis, officiis fugit temporibus reiciendis. Beatae nobis molestias tempore omnis consequuntur hic qui ducimus iure nostrum. Aperiam ab magnam sit iusto excepturi perferendis aliquid, recusandae autem ipsum accusantium, itaque aliquam laborum est quidem facere culpa, atque a numquam neque fugiat delectus. Dicta inventore molestiae ipsa hic vero explicabo temporibus dolores fuga voluptatibus. </p>
+            <!-- <a href="#" class="button">Read More</a> -->
+          </div>
+  
+    </div>
+    <!-- -->
+  
+    <div class="card">
+      <!-- <img src="Images/SJ.JPG" alt=""> -->
+      <div class="content">
+          <h2>S&J Inventory</h2>
+          <img src="Images/SJ.JPG" alt="">
+          <p>Quam iusto tenetur blanditiis dolores eos doloribus, odio numquam architecto alias amet consequatur, quae eius corporis, officiis fugit temporibus reiciendis. Beatae nobis molestias tempore omnis consequuntur hic qui ducimus iure nostrum. Aperiam ab magnam sit iusto excepturi perferendis aliquid, recusandae autem ipsum accusantium, itaque aliquam laborum est quidem facere culpa, atque a numquam neque fugiat delectus. Dicta inventore molestiae ipsa hic vero explicabo temporibus dolores fuga voluptatibus. </p>
+          <!-- <a href="#" class="button">Read More</a> -->
+        </div>
+  
+  </div>
+  <!-- -->
+  
+  <div class="card">
+    <!-- <img src="Images/SJ.JPG" alt=""> -->
+    <div class="content">
+        <h2>Google Drive Clone</h2>
+        <img src="Images/DriveClone.JPG" alt="">
+        <p>Quam iusto tenetur blanditiis dolores eos doloribus, odio numquam architecto alias amet consequatur, quae eius corporis, officiis fugit temporibus reiciendis. Beatae nobis molestias tempore omnis consequuntur hic qui ducimus iure nostrum. Aperiam ab magnam sit iusto excepturi perferendis aliquid, recusandae autem ipsum accusantium, itaque aliquam laborum est quidem facere culpa, atque a numquam neque fugiat delectus. Dicta inventore molestiae ipsa hic vero explicabo temporibus dolores fuga voluptatibus. </p>
+        <!-- <a href="#" class="button">Read More</a> -->
+      </div>
+  
+  </div>
+  <!-- -->
+  
+      </div><!-- Cards-->
+    </div>
+
+
+    <!-- PORTFOLIO-->
+
+
+    <section id="education">
+      <div class="toggleArea" onclick="showOrHideDiv()">
+        <!-- <img src="Images/toggle.png"> -->
+        <h2 class="toggle"><img id="toggleImage" src="Images/toggle.png"> Education</h2>
+      </div>
+      <div id="showOrHide">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid hic aperiam ad quae, quia officia sit. Officiis qui deserunt iure eaque hic illum expedita, ratione repellat molestias atque iste velit, delectus ut magni sit ab ducimus dolore nobis, eius ullam eveniet? Quam, at adipisci quae delectus quia eveniet officia. Quam iusto tenetur blanditiis dolores eos doloribus, odio numquam architecto alias amet consequatur, quae eius corporis, officiis fugit temporibus reiciendis. Beatae nobis molestias tempore omnis consequuntur hic qui ducimus iure nostrum. Aperiam ab magnam sit iusto excepturi perferendis aliquid
+
+
+        There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. 
+      </div>
+
+      <div class="toggleArea" onclick="showOrHideDiv2()">
+        <!-- <img src="Images/toggle.png"> -->
+        <h2 class="toggle"><img id="toggleImage2" src="Images/toggle.png"> Personal Life</h2>
+      </div>
+      <div id="showOrHide2">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid hic aperiam ad quae, quia officia sit. Officiis qui deserunt iure eaque hic illum expedita, ratione repellat molestias atque iste velit, delectus ut magni sit ab ducimus dolore nobis, eius ullam eveniet? Quam, at adipisci quae delectus quia eveniet officia. Quam iusto tenetur blanditiis dolores eos doloribus, odio numquam architecto alias amet consequatur, quae eius corporis, officiis fugit temporibus reiciendis. Beatae nobis molestias tempore omnis consequuntur hic qui ducimus iure nostrum. Aperiam ab magnam sit iusto excepturi perferendis aliquid
+
+
+        There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. 
+      </div>
+
+      <div class="toggleArea" onclick="showOrHideDiv()">
+        <!-- <img src="Images/toggle.png"> -->
+        <h2 class="toggle"><img id="toggleImage" src="Images/toggle.png"> Professional Experience</h2>
+      </div>
+      <div id="showOrHide">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid hic aperiam ad quae, quia officia sit. Officiis qui deserunt iure eaque hic illum expedita, ratione repellat molestias atque iste velit, delectus ut magni sit ab ducimus dolore nobis, eius ullam eveniet? Quam, at adipisci quae delectus quia eveniet officia. Quam iusto tenetur blanditiis dolores eos doloribus, odio numquam architecto alias amet consequatur, quae eius corporis, officiis fugit temporibus reiciendis. Beatae nobis molestias tempore omnis consequuntur hic qui ducimus iure nostrum. Aperiam ab magnam sit iusto excepturi perferendis aliquid
+
+
+        There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. 
+      </div>
+
+      <div class="toggleArea" onclick="showOrHideDiv2()">
+        <!-- <img src="Images/toggle.png"> -->
+        <h2 class="toggle"><img id="toggleImage2" src="Images/toggle.png"> Volounteer Experience</h2>
+      </div>
+      <div id="showOrHide2">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid hic aperiam ad quae, quia officia sit. Officiis qui deserunt iure eaque hic illum expedita, ratione repellat molestias atque iste velit, delectus ut magni sit ab ducimus dolore nobis, eius ullam eveniet? Quam, at adipisci quae delectus quia eveniet officia. Quam iusto tenetur blanditiis dolores eos doloribus, odio numquam architecto alias amet consequatur, quae eius corporis, officiis fugit temporibus reiciendis. Beatae nobis molestias tempore omnis consequuntur hic qui ducimus iure nostrum. Aperiam ab magnam sit iusto excepturi perferendis aliquid
+
+
+        There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. 
+      </div>
+
+
+
+
+
+
+
+    </section>
+
+    
+
+
+
+
+    <section id="contact">
+        <h1 class="heading1">Contact</h1>
+        <form action="" class="form">
+            <input type="text" name="name" class="input" placeholder="Enter Your Name">
+            <input type="email" name="email" class="input" placeholder="Enter Your Email">
+            <textarea name="message" id="message" cols="70" rows="20" placeholder="Enter Your Message"></textarea>
+            <input type="submit" value="Submit" id="submit">
+        </form>
+    </section>
+</body>
+</html>
+
+<?php include 'script.js';?>
+
+<script>
+  function showOrHideDiv() {
+     var toggle = document.getElementById("showOrHide");
+     var toggleImage  = document.getElementById("toggleImage");
+
+
+     if (toggle.style.display === "block") {
+      toggle.style.display = "none";
+      // toggleImage.style.width = "22px";
+      toggleImage.style.transform = "rotate(0deg)";
+     } else {
+      toggle.style.display = "block";
+      // toggleImage.style.width = "40px";
+      toggleImage.style.transform = "rotate(90deg)";
+     }
+  }
+
+  function showOrHideDiv2() {
+     var toggle = document.getElementById("showOrHide2");
+     var toggleImage  = document.getElementById("toggleImage2");
+
+
+     if (toggle.style.display === "block") {
+      toggle.style.display = "none";
+      // toggleImage.style.width = "22px";
+      toggleImage.style.transform = "rotate(0deg)";
+     } else {
+      toggle.style.display = "block";
+      // toggleImage.style.width = "40px";
+      toggleImage.style.transform = "rotate(90deg)";
+     }
+  }
+</script>
---
+++ script.js
@@ -0,0 +1,10 @@
+//Responsive Navbar
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+
+toggleButton.addEventListener('click', () => {
+        navbarLinks.classList.toggle('active')
+});
+//Responsive Navbar
+
+
