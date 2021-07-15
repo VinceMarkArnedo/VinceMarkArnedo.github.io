0d5dbdbc9754551ece177bd52630a6ed4212cc1b
VinceMarkArnedo <59438164+VinceMarkArnedo@users.noreply.github.com>
2021-07-15 14:47:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,55 @@ toggleButton.addEventListener('click', () => {
 //Responsive Navbar
 
 
+
+
+//ToggleShow About
+function showOrHideDiv() {
+        var toggle = document.getElementById("showOrHide");
+        var toggleImage  = document.getElementById("toggleImage");
+ 
+ 
+        if (toggle.style.display === "block") {
+         toggle.style.display = "none";
+         // toggleImage.style.width = "22px";
+         toggleImage.style.transform = "rotate(0deg)";
+        } else {
+         toggle.style.display = "block";
+         // toggleImage.style.width = "40px";
+         toggleImage.style.transform = "rotate(90deg)";
+        }
+     }
+ 
+     function showOrHideDiv2() {
+        var toggle = document.getElementById("showOrHide2");
+        var toggleImage  = document.getElementById("toggleImage2");
+ 
+ 
+        if (toggle.style.display === "block") {
+         toggle.style.display = "none";
+         // toggleImage.style.width = "22px";
+         toggleImage.style.transform = "rotate(0deg)";
+        } else {
+         toggle.style.display = "block";
+         // toggleImage.style.width = "40px";
+         toggleImage.style.transform = "rotate(90deg)";
+        }
+     }
+
+     function showOrHideDiv3() {
+        var toggle = document.getElementById("showOrHide3");
+        var toggleImage  = document.getElementById("toggleImage3");
+ 
+ 
+        if (toggle.style.display === "block") {
+         toggle.style.display = "none";
+         // toggleImage.style.width = "22px";
+         toggleImage.style.transform = "rotate(0deg)";
+        } else {
+         toggle.style.display = "block";
+         // toggleImage.style.width = "40px";
+         toggleImage.style.transform = "rotate(90deg)";
+        }
+     }
+
+
