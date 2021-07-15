ee403b3ec617c95512954fb7ace94b8c3f59d3bf
VinceMarkArnedo <59438164+VinceMarkArnedo@users.noreply.github.com>
2021-07-15 08:22:32 +0800

    LARGE

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio</title>
+    <!-- <link rel="stylesheet" href="Navbar A.css"> -->
+    <!-- <link rel="stylesheet" href="css/style.css"> -->
+    <link rel="stylesheet" href="css/card_style.css">
+    <link rel="stylesheet" href="css/toggleShow.css">
+    <link rel="stylesheet" href="css/contact.css">
+    <link rel="stylesheet" href="css/about.css">
+    <!-- <link rel="stylesheet" href="css/ResponsiveNavbar.css"> -->
+    <link rel="stylesheet" media="screen and (max-width:1170px)" href="css/tablet.css">
+    <link rel="stylesheet" media="screen and (max-width:900px)" href="css/phone.css">
+    <link rel="stylesheet" media="screen and (max-width:695px)" href="css/phone1.css">
+</head>
+<body>
+
+      <!-- Navbar-->
+          <?php include 'ResponsiveNavbar.html';?>
+      <!-- Navbar-->
+
+      <!-- Introduction-->
+      <?php include 'Introduction - Copy.html';?>
+      <!-- Introduction-->
+
+      <!-- Projects-->
+      <?php include 'Projects.html';?>
+      <!-- Projects-->
+
+      <!-- About Me-->
+      <?php include 'About.html';?>
+      <!-- About Me-->
+
+      <!-- Contact-->
+      <?php include 'Contact.html';?>
+      <!-- Contact-->
+
+
+</body>
+</html>
+
+
+<!-- Script-->
+<script  type="text/javascript" src="script.js"></script>
+<!-- Script-->
